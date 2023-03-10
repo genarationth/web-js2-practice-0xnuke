0cf784294f66de11d900fef51c85a153958ac54a
0xnuke <116099850+0xnuke@users.noreply.github.com>
2023-03-11 00:27:11 +0700

    Add files via upload
    add challenge 2

---
+++ Challenge1.js
@@ -0,0 +1,7 @@
+let card = {
+    name:"John",
+    location:"South Africa",
+    hobby:"drinking homemade wine"
+  }
+  
+  console.log(`Hi, my name is ${card.name}. I live in ${card.location} and ${card.hobby}`)
