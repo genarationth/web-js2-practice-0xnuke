c36fe370f231d9daef52d6719191fdfb258dde6d
0xnuke <116099850+0xnuke@users.noreply.github.com>
2023-03-11 01:39:11 +0700

    Update Challenge1.js
    add text in console

--- Challenge1.js
+++ Challenge1.js
@@ -1,4 +1,4 @@
 let birthYears = 1988;
 let futureYears = 2026;
 let yourAges = futureYears-birthYears;
-console.log(yourAges);
+console.log(`If you were born in ${birthYears}, then in ${futureYears} you’ll be ${yourAges - 1} or ${yourAges} (depending on the month in ${futureYears}).`);
